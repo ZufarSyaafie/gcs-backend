a46b2c73f43baceb9f0abd7e877c0349e825557d
ZufarSyaafie <zufar.syaafie@gmail.com>
2024-10-25 16:48:16 +0700

    Delete CORS v2

--- server.js
+++ server.js
@@ -1,23 +1,18 @@
 const express = require("express");
 const http = require("http");
+const cors = require("cors");
 const { setupWebsocket, generateRandomData } = require("./websocket");
 
 const app = express();
+app.use(cors());
 
 const server = http.createServer(app);
 
-// Middleware to set Access-Control-Allow-Origin header
-app.use((req, res, next) => {
-	res.header("Access-Control-Allow-Origin", "https://zufarsyaafie.github.io");
-	res.header("Access-Control-Allow-Methods", "GET, POST");
-	next();
-});
-
 const io = require("socket.io")(server, {
-	cors: {
-		origin: "https://zufarsyaafie.github.io",
-		methods: ["GET", "POST"],
-	},
+	// cors: {
+	// 	origin: "https://zufarsyaafie.github.io",
+	// 	methods: ["GET", "POST"],
+	// },
 });
 setupWebsocket(io);
 
