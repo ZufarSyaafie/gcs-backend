125fee6da746aa3a99dd94f12f62332c0ae0e7bd
ZufarSyaafie <zufar.syaafie@gmail.com>
2024-10-25 16:20:23 +0700

    Change the origin

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const server = http.createServer(app);
 
 const io = require("socket.io")(server, {
 	cors: {
-		origin: "https://zufarsyaafie.github.io/gcs-project/",
+		origin: "https://zufarsyaafie.github.io",
 		methods: ["GET", "POST"],
 	},
 });
