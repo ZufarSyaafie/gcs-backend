85e0301e060f368363b4eecb12dd4bf67a4beb89
ZufarSyaafie <zufar.syaafie@gmail.com>
2024-10-25 16:14:04 +0700

    Change the origin

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const server = http.createServer(app);
 
 const io = require("socket.io")(server, {
 	cors: {
-		origin: "https://gcs-project.vercel.app/",
+		origin: "https://zufarsyaafie.github.io/gcs-project/",
 		methods: ["GET", "POST"],
 	},
 });
